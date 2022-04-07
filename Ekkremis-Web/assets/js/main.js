d21d10c2b442515ad22c6d1e7aa278cb4f39effb
Nathaniel Peñaranda <53138333+Nathaniel121215@users.noreply.github.com>
2022-04-07 17:44:12 +0800

    Scroll up Update

--- Ekkremis-Web/assets/js/main.js
+++ Ekkremis-Web/assets/js/main.js
@@ -1,7 +1,7 @@
 
 
 
-const sr = ScrollReveal({     distance: '60px',     duration: 2500,     delay: 100, reset: true })
+const sr = ScrollReveal({     distance: '60px',     duration: 2500,     delay: 100})
 
 /*===== INDEX =====*/ 
 
@@ -131,6 +131,15 @@ const toggleItem = (item) =>{
     }
 
 }
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 
